625aa1d68346d1848db05315dab989843250e33e
JonathanSouth <jonathan.south@mymona.uwi.edu>
2018-03-22 04:14:47 +0000

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# INFO3180 Lab 4 Starter Code
-This is the starter code for Lab 4 which is requires you to build a Flask app that does simple file uploads and has basic authentication.
+# INFO3180 Project 1 
--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Hxx88F4snT"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:info3180project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresl://dnmvbfumjeivou:6f0efcfb5282c8c6124e83a07e44f144ff129748a385dba08401abe7bb8d4496@ec2-107-20-233-240.compute-1.amazonaws.com:5432/d23p98m4k90en0"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
 db = SQLAlchemy(app)
